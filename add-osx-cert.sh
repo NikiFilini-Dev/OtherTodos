e979523512989fd03948b084a8c3bc0e26813f85
Lunavod <lunavod@bk.ru>
2021-05-01 00:40:09 +0300

    Fix identity in macos flow [3]

--- add-osx-cert.sh
+++ add-osx-cert.sh
@@ -1,23 +1,10 @@
 #!/usr/bin/env sh
 
-KEY_CHAIN=build.keychain
-CERTIFICATE_P12=certificate.p12
-
-# Recreate the certificate from the secure environment variable
-echo $MACOS_CERTIFICATE | base64 --decode > $CERTIFICATE_P12
-
-#create a keychain
-security create-keychain -p actions $KEY_CHAIN
-
-# Make the keychain the default so identities are found
-security default-keychain -s $KEY_CHAIN
-
-# Unlock the keychain
-security unlock-keychain -p actions $KEY_CHAIN
-
-security import $CERTIFICATE_P12 -k $KEY_CHAIN -P $MACOS_CERTIFICATE_PWD -T /usr/bin/codesign;
-
-security set-key-partition-list -S apple-tool:,apple: -s -k actions $KEY_CHAIN
+echo $MACOS_CERTIFICATE | base64 —decode > certificate.p12
+security create-keychain -p keychainPassword build.keychain security default-keychain -s build.keychain
+security unlock-keychain -p keychainPassword build.keychain
+security import certificate.p12 -k build.keychain -P $MACOS_CERTIFICATE_PWD -T /usr/bin/codesign
+security set-key-partition-list -S apple-tool:,apple:,codesign: -s -k keychainPassword build.keychain
 
 security find-identity
 
