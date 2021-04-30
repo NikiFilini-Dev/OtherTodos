6abbb4d97eb92664d05be9239a9c727c663a3b9c
Lunavod <lunavod@bk.ru>
2021-05-01 01:09:20 +0300

    Macos installer cert

--- add-osx-cert.sh
+++ add-osx-cert.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env sh
 
-echo $MACOS_CERTIFICATE | base64 --decode > certificate.p12
+echo $MACOS_CERTIFICATE_APP | base64 --decode > certificate_app.p12
+echo $MACOS_CERTIFICATE_INST | base64 --decode > certificate_inst.p12
+
 security create-keychain -p keychainPassword build.keychain
 security default-keychain -s build.keychain
 security unlock-keychain -p keychainPassword build.keychain
-security import certificate.p12 -k build.keychain -P $MACOS_CERTIFICATE_PWD -T /usr/bin/codesign
+
+security import certificate_app.p12 -k build.keychain -P $MACOS_CERTIFICATE_APP_PWD -T /usr/bin/codesign
+security import certificate_inst.p12 -k build.keychain -P $MACOS_CERTIFICATE_INST_PWD -T /usr/bin/codesign
+
 security set-key-partition-list -S apple-tool:,apple:,codesign: -s -k keychainPassword build.keychain
 
 security find-identity
